e04594ec75ba14eae3ea0a2a47baa33f3d6e0367
devonhackley <devonhackley@gmail.com>
2016-12-19 12:52:46 -0800

    added the google token into the token.js file

--- token.js
+++ token.js
@@ -1,1 +1,2 @@
-var token = '_7x37AIhk_hdml_p5LSrM_u3-O6wZnfHzwa6ZjykVSX-chGpwTNMqcBumtMCqqzOmyTmokrffDFiiqpmLCikz3c_eU3MjEM5E-I010wgsdbPZEiOMDr5Z7KbJEhUWHYx';
+var yelpToken = '_7x37AIhk_hdml_p5LSrM_u3-O6wZnfHzwa6ZjykVSX-chGpwTNMqcBumtMCqqzOmyTmokrffDFiiqpmLCikz3c_eU3MjEM5E-I010wgsdbPZEiOMDr5Z7KbJEhUWHYx';
+var googleToken ='AIzaSyCqHl1ZOIk0ob_jQZUPnaE4UVgd-UIo910';
