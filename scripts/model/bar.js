2165fdbf17e5a66cded742d7d3d569c8e3270ca1
Ashley Ross <ashley.e.ross@gmail.com>
2016-12-20 13:17:20 -0800

    data rendering according to logs

--- scripts/model/bar.js
+++ scripts/model/bar.js
@@ -4,20 +4,19 @@
 
   Bar.allBars = [];
 
-//I have a feeling we need to refactor this still -nikko
-  function Business(opts){
-    Object.keys(opts).forEach(function(element,index){
-      // this[element] = opts[element];
-      this.name = opts[index].name;
-      this.latitude =  opts[index].coordinates.latitude;
-      this.longitude = opts[index].coordinates.longitude;
-      this.address = opts[index].location.display_address;
-      this.phone = opts[index].display_phone;
-      this.image = opts[index].image_url;
-      this.price = opts[index].price;
-      this.rating = opts[index].rating;
-      this.currentlyOpen = opts[index].is_closed;
-    },this);
+  function RenderBusinesses(opts){
+    this.name = opts.name;
+    console.log(this.name);
+    this.latitude =  opts.coordinates.latitude;
+    this.longitude = opts.coordinates.longitude;
+    // note: address is an array of 3 items
+    this.address = opts.location.display_address;
+    this.phone = opts.display_phone;
+    this.image = opts.image_url;
+    this.price = opts.price;
+    this.rating = opts.rating;
+    this.currentlyOpen = opts.is_closed;
+    Bar.allBars.push(this);
   }
 
   Bar.requestData = function(){
@@ -25,13 +24,10 @@
       type: 'GET',
       url: '/yelp/v3/businesses/search?categories=bars&term=dogs%20allowed&location=98103&limit=50&sort_by=distance',
       success: function(data) {
-        data.businesses.forEach(function(bar){
-          var current_bar = new Business(bar);
-          Bar.allBars.push(current_bar);
-
+        data.businesses.forEach(function(item){
+          new RenderBusinesses(item);
         });
         console.table(Bar.allBars);
-
       }
     });
   };
