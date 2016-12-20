537ad4297ded73426a9ceba0ef861ae2184eec1d
devonhackley <devonhackley@gmail.com>
2016-12-20 12:41:53 -0800

    trying to create objects from the data pulled form yelp

--- scripts/model/bar.js
+++ scripts/model/bar.js
@@ -7,13 +7,10 @@
 //I have a feeling we need to refactor this still -nikko
   function Business(opts){
     Object.keys(opts).forEach(function(element,index){
-      this[element] = opts[element];
+      // this[element] = opts[element];
       this.name = opts[index].name;
-      this.location = {
-        latitude: this.latitude = opts[index].coordinates.latitude,
-        longitude: this.longitude = opts[index].coordinates.longitude
-      };
-      // this.displayAddress = opts[index].display_address[0] + ' ' +  opts[index].display_address[1] + ' ' + opts[index].display_address[2];
+      this.latitude =  opts[index].coordinates.latitude;
+      this.longitude = opts[index].coordinates.longitude;
       this.address = opts[index].location.display_address;
       this.phone = opts[index].display_phone;
       this.image = opts[index].image_url;
@@ -26,10 +23,13 @@
   Bar.requestData = function(){
     $.ajax({
       type: 'GET',
-      url: '/yelp/v3/businesses/search?categories=bars&term=dogs%20allowed&location=seattle&limit=50',
+      url: '/yelp/v3/businesses/search?categories=bars&term=dogs%20allowed&location=98103&limit=50&sort_by=distance',
       success: function(data) {
-        var bar = new Business(data.businesses);
-        Bar.allBars.push(bar);
+        data.businesses.forEach(function(bar){
+          var current_bar = new Business(bar);
+          Bar.allBars.push(current_bar);
+
+        });
         console.table(Bar.allBars);
 
       }
