f41901f3a118b6b17d97d8a4d4cd0e15c715e61c
Ashley Ross <ashley.e.ross@gmail.com>
2016-12-20 14:26:35 -0800

    data displaying in application

--- scripts/model/bar.js
+++ scripts/model/bar.js
@@ -7,7 +7,7 @@
   function RenderBusinesses(opts){
     this.name = opts.name;
     console.log(this.name);
-    this.latitude =  opts.coordinates.latitude;
+    this.latitude = opts.coordinates.latitude;
     this.longitude = opts.coordinates.longitude;
     // note: address is an array of 3 items
     this.address = opts.location.display_address;
@@ -15,9 +15,40 @@
     this.image = opts.image_url;
     this.price = opts.price;
     this.rating = opts.rating;
-    this.currentlyOpen = opts.is_closed;
+    this.closed = opts.is_closed;
     Bar.allBars.push(this);
   }
+  RenderBusinesses.createTable = function(){
+    // Bar.requestData();
+    webDB.execute(
+    'CREATE TABLE IF NOT EXISTS bars_database (' +
+      'id INTEGER PRIMARY KEY, ' +
+      'name VARCHAR NOT NULL, ' +
+      'latitude FLOAT NOT NULL, ' +
+      'longitude FLOAT NOT NULL, ' +
+      'address VARCHAR NOT NULL, ' +
+      'phone VARCHAR NOT NULL, ' +
+      'image VARCHAR NOT NULL, ' +
+      'price VARCHAR NOT NULL, ' +
+      'rating FLOAT NOT NULL, ' +
+      'closed BOOLEAN);',
+      function(){
+        console.log('table render successful');
+        Bar.requestData();
+      }
+  );
+  };
+
+  RenderBusinesses.prototype.insertRecord = function(){
+    webDB.execute(
+      [
+        {
+          'sql': 'INSERT INTO bars_database (name, latitude, longitude, address, phone, image, price, rating, closed) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);',
+          'data': [this.name, this.latitude, this.longitude, this.address, this.phone, this.image, this.price, this.rating, this.closed]
+        }
+      ]
+    );
+  };
 
   Bar.requestData = function(){
     $.ajax({
@@ -25,11 +56,13 @@
       url: '/yelp/v3/businesses/search?categories=bars&term=dogs%20allowed&location=98103&limit=50&sort_by=distance',
       success: function(data) {
         data.businesses.forEach(function(item){
-          new RenderBusinesses(item);
+          var sqlTable = new RenderBusinesses(item);
+          sqlTable.insertRecord();
         });
         console.table(Bar.allBars);
       }
     });
   };
+  RenderBusinesses.createTable();
   module.Bar = Bar;
 })(window);
