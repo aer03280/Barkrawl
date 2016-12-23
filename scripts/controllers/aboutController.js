a822bae83a96cf279c1784b03b5f81f2351196d5
Ashley Ross <ashley.e.ross@gmail.com>
2016-12-22 23:38:45 -0800

    bar form styles

--- scripts/controllers/aboutController.js
+++ scripts/controllers/aboutController.js
@@ -5,6 +5,7 @@
     $('#about-view').show();
     $('#map-view').hide();
     $('#user-form-container').hide();
+    $('#bar-view').hide();
     console.log('This is a meaningful message');
   };
   module.aboutController = aboutController;
